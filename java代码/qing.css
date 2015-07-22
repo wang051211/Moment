1b2c984d0031f0a90b8d056808c4d599c05e2d24
wang051211 <602025574@qq.com>
2015-07-22 20:25:40 +0800

    files

---
+++ java代码/qing.css
@@ -0,0 +1,8 @@
+public class wang{
+	public static void main(String[] args){
+		String name="¹ş¹ş";
+		int age=21;
+		System.out.println("ĞÕÃû£º"+name);
+		System.out.print("ÄêÁä£º"+age);
+	}
+}
---
+++ java代码/qing.java
@@ -0,0 +1,6 @@
+public class qing{
+	public static void main(String[] args){
+		String name="哈哈";
+		System.out.println("姓名:"+name);
+   }
+}
